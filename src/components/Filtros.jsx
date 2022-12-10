0828e8e19b0c2a2c4b7fb8835bb948d19be3de48
Pablo Valente <pablo.nvalente@gmail.com>
2022-12-10 13:19:07 -0300

    add product filter

--- src/components/Filtros.jsx
+++ src/components/Filtros.jsx
@@ -4,8 +4,12 @@ import { BsFilterRight } from 'react-icons/bs';
 import { HiChevronDown } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 
-const Filtros = ({ productos }) => {
+const Filtros = ({ productos, setAromaElegido }) => {
   const [aromas, setAromas] = useState([]);
+
+  const handleSetAroma = (e) => {
+    setAromaElegido(e.target.innerHTML);
+  };
 
   useEffect(() => {
     const aromasUnicos = new Set(productos.map((producto) => producto.aroma));
@@ -21,55 +25,81 @@ const Filtros = ({ productos }) => {
       </Popover.Button>
       <Popover.Overlay className='fixed inset-0 bg-black/30' />
       <Popover.Panel className='absolute bg-white overflow-hidden rounded-md shadow w-52 right-0 top-8 divide-y divide-neutral-200'>
-        <Disclosure as='div'>
-          {({ open }) => (
-            <>
-              <Disclosure.Button
-                className={`flex items-center justify-between w-full font-bold text-sm uppercase tracking-wider py-3 px-4 ${
-                  open ? 'shadow-md' : ''
-                }`}>
-                <span>Linea</span>
-                <HiChevronDown className={open ? 'rotate-180 transition' : 'rotate-0 transition'} />
-              </Disclosure.Button>
-              <Disclosure.Panel>
-                <ul className='text-zinc-500 bg-neutral-100 space-y-3 p-4'>
-                  <li>
-                    <Link to='/productos/velas'>Velas</Link>
-                  </li>
-                  <li>
-                    <Link to='/productos/difusores'>Difusores</Link>
-                  </li>
-                  <li>
-                    <Link to='/productos/home-spray'>Home Spray</Link>
-                  </li>
-                </ul>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+        {({ close }) => (
+          <>
+            <Disclosure as='div'>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    className={`flex items-center justify-between w-full font-bold text-sm uppercase tracking-wider py-3 px-4 ${
+                      open ? 'shadow-md' : ''
+                    }`}>
+                    <span>Linea</span>
+                    <HiChevronDown
+                      className={open ? 'rotate-180 transition' : 'rotate-0 transition'}
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel>
+                    <ul className='text-zinc-500 bg-neutral-100 space-y-3 p-4'>
+                      <li>
+                        <Link to='/productos/velas'>Velas</Link>
+                      </li>
+                      <li>
+                        <Link to='/productos/difusores'>Difusores</Link>
+                      </li>
+                      <li>
+                        <Link to='/productos/home-spray'>Home Spray</Link>
+                      </li>
+                    </ul>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
 
-        <Disclosure as='div'>
-          {({ open }) => (
-            <>
-              <Disclosure.Button
-                className={`flex items-center justify-between w-full font-bold text-sm uppercase tracking-wider py-3 px-4 ${
-                  open ? 'shadow' : ''
-                }`}>
-                <span>Aroma</span>
-                <HiChevronDown className={open ? 'rotate-180 transition' : 'rotate-0 transition'} />
-              </Disclosure.Button>
-              <Disclosure.Panel>
-                <ul className='text-zinc-500 bg-neutral-100 space-y-3 p-4'>
-                  {aromas.map((aroma) => (
-                    <li key={aroma} className='capitalize'>
-                      {aroma}
-                    </li>
-                  ))}
-                </ul>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+            <Disclosure as='div'>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    className={`flex items-center justify-between w-full font-bold text-sm uppercase tracking-wider py-3 px-4 ${
+                      open ? 'shadow' : ''
+                    }`}>
+                    <span>Aroma</span>
+                    <HiChevronDown
+                      className={open ? 'rotate-180 transition' : 'rotate-0 transition'}
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel>
+                    {({ close }) => (
+                      <ul className='text-zinc-500 bg-neutral-100 space-y-3 p-4'>
+                        {aromas.map((aroma) => (
+                          <li
+                            key={aroma}
+                            className='capitalize'
+                            onClick={(e) => {
+                              handleSetAroma(e);
+                              close();
+                            }}>
+                            {aroma}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+            <div className='px-4 py-2'>
+              <button
+                onClick={() => {
+                  setAromaElegido('');
+                  close();
+                }}
+                className='uppercase ml-auto text-[10px] flex gap-1 items-center font-bold text-red-800'>
+                Quitar filtros
+              </button>
+            </div>
+          </>
+        )}
       </Popover.Panel>
     </Popover>
   );
--- src/components/ProductListContainer/ProductList.jsx
+++ src/components/ProductListContainer/ProductList.jsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import BackButton from '../BackButton';
 import Filtros from '../Filtros';
 import ProductItem from './ProductItem';
 
 const ProductList = ({ productos }) => {
+  const [aromaElegido, setAromaElegido] = useState('');
+
   return (
     <>
       <div className='px-4 flex items-center justify-between'>
         <BackButton />
-        <Filtros productos={productos} />
+        <Filtros productos={productos} setAromaElegido={setAromaElegido} />
       </div>
-      <ul className='grid grid-cols-2 md:grid-cols-4 gap-4 p-4'>
-        {productos.map((product) => (
-          <ProductItem key={product.nombre} product={product} />
-        ))}
-      </ul>
+      {aromaElegido ? (
+        <ul className='grid grid-cols-2 md:grid-cols-4 gap-4 p-4'>
+          {productos
+            .filter((producto) => producto.aroma === aromaElegido)
+            .map((product) => (
+              <ProductItem key={product.nombre} product={product} />
+            ))}
+        </ul>
+      ) : (
+        <ul className='grid grid-cols-2 md:grid-cols-4 gap-4 p-4'>
+          {productos.map((product) => (
+            <ProductItem key={product.nombre} product={product} />
+          ))}
+        </ul>
+      )}
     </>
   );
 };
