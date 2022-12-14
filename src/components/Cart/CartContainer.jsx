af5cbe06b06e6a8745d84dd9d098e213c1be3205
Pablo Valente <pablo.nvalente@gmail.com>
2022-12-14 14:15:41 -0300

    trying stuff

--- src/components/Cart/CartContainer.jsx
+++ src/components/Cart/CartContainer.jsx
@@ -18,9 +18,14 @@ const CartContainer = () => {
       <div
         className={`${
           open ? 'translate-x-0' : 'translate-x-[600px]'
-        } transition duration-300 delay-150 ease-out fixed z-50 top-0 right-0 w-3/4 h-screen bg-zinc-200 shadow-xl flex flex-col`}>
+        } transition duration-300 delay-150 ease-out fixed z-50 top-0 right-0 w-3/4 h-[90%] bg-zinc-200 shadow-xl flex flex-col`}>
         <div className='bg-zinc-900 text-zinc-300 px-4 py-3 font-semibold text-center text-sm'>
           Carrito de compras
+        </div>
+
+        <div className='flex justify-between items-center bg-white border-t px-4 py-3 mb-1.5 shadow-md'>
+          <p>Precio total:</p>
+          <p>$10.250</p>
         </div>
 
         <div className='overflow-y-scroll grow grid gap-1.5'>
@@ -30,11 +35,6 @@ const CartContainer = () => {
           <CartItem />
           <CartItem />
           <CartItem />
-        </div>
-
-        <div className='flex justify-between items-center bg-white border-t px-4 pt-3 pb-10 reverse-shadow-md'>
-          <p>Precio total:</p>
-          <p>$10.250</p>
         </div>
       </div>
     </>
