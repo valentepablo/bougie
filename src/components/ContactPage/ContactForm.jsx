8d2c25cc31fd4510dd65ab16b730bfa036eea33a
Pablo Valente <pablo.nvalente@gmail.com>
2022-12-17 11:57:56 -0300

    changes form

--- src/components/ContactPage/ContactForm.jsx
+++ src/components/ContactPage/ContactForm.jsx
@@ -1,5 +1,11 @@
 import { useFormik } from 'formik';
-import { HiOutlineUser, HiOutlineEnvelope, HiOutlineDevicePhoneMobile } from 'react-icons/hi2';
+import * as Yup from 'yup';
+import {
+  HiOutlineUser,
+  HiOutlineEnvelope,
+  HiOutlineDevicePhoneMobile,
+  HiPaperAirplane,
+} from 'react-icons/hi2';
 
 const ContactForm = () => {
   const formik = useFormik({
@@ -9,10 +15,26 @@ const ContactForm = () => {
       telefono: '',
       mensaje: '',
     },
+    validationSchema: Yup.object({
+      nombre: Yup.string()
+        .min(3, 'Debe tener al menos 3 caracteres')
+        .max(25, 'Debe tener un maximo de 25 caracteres')
+        .required('Debes ingresar tu nombre'),
+      email: Yup.string()
+        .email('Ingresa un correo electronico valido')
+        .required('Debes ingresar un correo electronico'),
+      telefono: Yup.number().min(8).max(14).optional(),
+      mensaje: Yup.string()
+        .min(10, 'Debe tener al menos 10 caracteres')
+        .max(250, 'Limite de caracteres alcanzado')
+        .required('Debes ingresar un mensaje para enviar'),
+    }),
     onSubmit: (values) => {
       console.log(values);
+      formik.resetForm();
     },
   });
+  console.log(formik.errors);
 
   return (
     <form className='space-y-4 mt-2' onSubmit={formik.handleSubmit}>
@@ -23,7 +45,7 @@ const ContactForm = () => {
         <input
           type='text'
           name='nombre'
-          placeholder='Ingresa tu nombre completo'
+          placeholder='*Ingresa tu nombre completo'
           className='w-full rounded-md py-2 pl-10 pr-3 bg-zinc-50 border placeholder:text-sm placeholder:text-zinc-400'
           onChange={formik.handleChange}
           value={formik.values.nombre}
@@ -38,8 +60,10 @@ const ContactForm = () => {
         <input
           type='email'
           name='email'
-          placeholder='Ingresa un correo electronico'
+          placeholder='*Ingresa un correo electronico'
           className='w-full rounded-md py-2 pl-10 pr-3 bg-zinc-50 border placeholder:text-sm placeholder:text-zinc-400'
+          onChange={formik.handleChange}
+          value={formik.values.email}
         />
       </div>
 
@@ -50,23 +74,35 @@ const ContactForm = () => {
         <input
           type='tel'
           name='telefono'
-          placeholder='Ingresa tu numero de contacto'
+          placeholder='Ingresa un numero de contacto'
           className='w-full rounded-md py-2 pl-10 pr-3 bg-zinc-50 border placeholder:text-sm placeholder:text-zinc-400'
+          onChange={formik.handleChange}
+          value={formik.values.telefono}
         />
       </div>
 
-      <div>
+      <div className='relative'>
         <textarea
           name='mensaje'
           rows='5'
-          placeholder='Escribe un mensaje...'
-          className='w-full rounded-md bg-zinc-50 border placeholder:text-sm placeholder:text-zinc-400 p-3'></textarea>
+          maxLength='250'
+          placeholder='*Escribe un mensaje...'
+          className='w-full rounded-md bg-zinc-50 border placeholder:text-sm placeholder:text-zinc-400 p-3 py-4'
+          onChange={formik.handleChange}
+          value={formik.values.mensaje}></textarea>
+
+        <span className='absolute top-0 right-1 text-[10px] text-zinc-400'>
+          {250 - formik.values.mensaje.length} / 250
+        </span>
       </div>
+
+      <span className='italic text-zinc-400 text-xs'>* : Campos obligatorios.</span>
 
       <button
         type='submit'
-        className='w-full rounded-md bg-black text-zinc-200 uppercase text-xs font-bold h-12'>
+        className='flex items-center justify-center gap-1 w-full rounded-md bg-black text-zinc-200 uppercase text-xs font-bold h-12'>
         Enviar
+        <HiPaperAirplane />
       </button>
     </form>
   );
