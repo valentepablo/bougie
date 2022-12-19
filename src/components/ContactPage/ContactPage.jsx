0d32444f75124b255aff02be873a0e9150ed6370
Pablo Valente <pablo.nvalente@gmail.com>
2022-12-19 17:17:53 -0300

    add contact ig link

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -1,3 +1,4 @@
+import { BsInstagram } from 'react-icons/bs';
 import Section from '../Section/Section';
 import SectionBody from '../Section/SectionBody';
 import SectionHeader from '../Section/SectionHeader';
@@ -12,6 +13,21 @@ const ContactPage = () => {
       </SectionHeader>
       <SectionBody>
         <ContactUs />
+        <div className='border-t my-3'></div>
+        <div className='text-zinc-600 text-sm'>
+          <p className='flex items-center justify-center gap-1'>
+            Encontranos tambien en{' '}
+            <a
+              href='https://www.instagram.com/bougieshopoficial'
+              target='_blank'
+              className='font-extrabold underline flex items-center gap-1'>
+              Instagram
+              <span>
+                <BsInstagram />
+              </span>
+            </a>
+          </p>
+        </div>
       </SectionBody>
     </Section>
   );
